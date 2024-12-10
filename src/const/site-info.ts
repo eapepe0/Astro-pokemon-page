745e6764ba9f2cf8ce002fab6fb8d190e8d3b95e
eapepe0 <eapepe0@gmail.com>
2024-12-09 21:30:09 -0300

    metadatos meta description meta author , og:image og:title og:description

---
+++ src/const/site-info.ts
@@ -0,0 +1,8 @@
+export const siteInfo = {
+    title : "Pokemon Static",
+    description : "A static site with Pokemon data, powered by pokeapi.co",
+    image : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png",
+    author : "Cristian Oyola"
+}
+
+// creamos unas constantes con la informacion del sitio
