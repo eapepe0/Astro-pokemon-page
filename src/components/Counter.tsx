2acfcec76b7e23ffd9f30d2152fbb1ed500e22c9
eapepe0 <eapepe0@gmail.com>
2024-12-14 23:45:13 -0300

    53.Utiliceel transition:persist , para que un valor se mantenga entre transiciones , y para que se comparta entre componentes con el transition:persist-props , y poder compartir <Componentes/> entre islas , y como agregar un children en los props el cual es un JSX.Element

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,23 @@
-import { createSignal } from "solid-js"
+import { createSignal, type Component, type JSX } from "solid-js"
 // el createSignal es como el useState de React
 
-export const Counter = () => {
 
-    const [counter , setCounter] = createSignal(10)
+// creamos una interface con las props que vamos a utilizar
+interface Props {
+  initValue : number;
+  children? : JSX.Element; /* recibimos un children el cual es un elemento JSX*/
+}
+
+// Counter : es un Component que recibe unas <Props> la cual  recibimos en props
+export const Counter: Component<Props> = (props) => {
+
+    // usamos el initValue como valor inicial
+    const [counter , setCounter] = createSignal(props.initValue)
 
 
   return (
     <div class="space-y-5">
-        <h1 class="text-4xl">Counter</h1>
+        {props.children}
         <h3 class="text-xl">Value : {counter()}</h3>
 
         <button onClick={()=> setCounter((prev)=> ++prev)} class="bg-blue-500 p-2 mr-2 rounded">+1</button>
